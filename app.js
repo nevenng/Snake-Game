2ad0818cfa7a747340cdc6005517df7cc4d217b3
Neven Nguyen <nevenng@gmail.com>
2023-03-19 23:15:10 -0700

    starting files

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+/*start the game by pressing a Start button
+use my arrow keys to change the direction of the snake
+have the snake grow correctly when it eats the apple
+have the game end if the snake tries to eat itself
+have the game end if the snake runs into a wall
+see how long my snake was when the game ended
+start the game over without having to reset the browser
+*/
+
+
+//move snake function
+
+// apple random generator
+
+// snake tail lengthen
+
+//player 1 and player 2 values & input
+
+//check for hits (wall)
